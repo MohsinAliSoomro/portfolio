700dea60b3ad4e1737bab38dc20f8da970453951
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2025-02-06 11:26:58 +0500

    change done

--- app/routes/home/profile.jsx
+++ app/routes/home/profile.jsx
@@ -21,14 +21,14 @@ const ProfileText = ({ visible, titleId }) => (
       <DecoderText text="Hi there" start={visible} delay={500} />
     </Heading>
     <Text className={styles.description} data-visible={visible} size="l" as="p">
-      I’m Hamish, currently I live in Sydney working as a senior product designer at{' '}
-      <Link href="https://www.qwilr.com">Qwilr</Link>. My projects include UX design, UI
-      animations, and icon illustration. Being comfortable with code allows me to rapidly
-      prototype and validate experiences. If you’re interested in the tools and software I
-      use check out my <Link href="/uses">uses page</Link>.
+      I’m Mohsin Ali, currently I live in Pakistan working as a Software Engineer at{' '}
+      <Link href="https://www.alliancecaretech.com/">Alliance care technology</Link>. My
+      projects include Software engieer, UI design, and Mobile App. Being comfortable with
+      code allows me to rapidly prototype and validate experiences. If you’re interested
+      in the tools and software I use check out my <Link href="/uses">uses page</Link>.
     </Text>
     <Text className={styles.description} data-visible={visible} size="l" as="p">
-      In my spare time I like to practice Brazilian Jiu Jitsu, play video games, and{' '}
+      In my spare time I like to practice Hiking, play video games, and{' '}
       <Link href="/projects/volkihar-knight">make mods</Link>. I’m always down for hearing
       about new projects, so feel free to drop me a line.
     </Text>
